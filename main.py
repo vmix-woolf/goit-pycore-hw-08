6f2ed696cf72e927771fd8f6c100cb11bbbb1893
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2024-10-24 10:25:02 +0300

    add addressbook.pkl in .gitignore, little refactoring

--- main.py
+++ main.py
@@ -46,13 +46,14 @@ def main():
 
 
 def save_data(book, filename="addressbook.pkl"):
-    with open(filename, "wb") as f:
-        pickle.dump(book, f)
+    with open(filename, "wb") as fh:
+        pickle.dump(book, fh)
+
 
 def load_data(filename="addressbook.pkl"):
     try:
-        with open(filename, "rb") as f:
-            return pickle.load(f)
+        with open(filename, "rb") as fh:
+            return pickle.load(fh)
     except FileNotFoundError:
         return AddressBook()
     
